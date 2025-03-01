3baaca1a030031c626ec7ec4f597f6e2acb4b238
vidhu <vidhudash@gmail.com>
2025-03-02 00:34:33 +0530

    feat: introduce blocked cells with random data

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"math/rand"
+    "bufio"
+    "strconv"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -13,12 +16,40 @@ type model struct {
     board [][]rune
 }
 
+type cell struct {
+    x, y int
+}
+
 func initModel() model {
+    file, err := os.Open("./assets/cw.info")
+    if err != nil {
+        panic(err)
+    }
+    defer file.Close()
+    // defer func() {
+    //     file.Close()
+    // }()
+
+    scanner := bufio.NewScanner(file)
+    blockedCell := make(map[cell]struct{})
+    for scanner.Scan() {
+        var c cell
+        coords := strings.Split(scanner.Text(), ",")
+        c.x, _ = strconv.Atoi(coords[0])
+        c.y, _ = strconv.Atoi(coords[1])
+        blockedCell[c] = struct{}{}
+    }
+
     _board := make([][]rune, 15)
+    min, max := 65, 90
     for i := range 15 {
         _board[i] = make([]rune, 15)
         for j := range 15 {
-            _board[i][j] = '$'
+            if _, exists := blockedCell[cell{i, j}]; !exists {
+                _board[i][j] = rune(rand.Intn(max - min + 1) + min)
+            } else {
+                _board[i][j] = '$'
+            }
         }
     }
     return model {
@@ -57,7 +88,11 @@ func (m model) View() string {
         }
         s.WriteString("\n\t")
         for j := range 15 {
-            s.WriteString(boardStyle.Render("┃  " + string(m.board[i][j]) + " "))
+            if m.board[i][j] == '$' {
+                s.WriteString(boardStyle.Render("┃") + "    ")
+            } else {
+                s.WriteString(boardStyle.Render("┃  " + string(m.board[i][j]) + " "))
+            }
         }
         s.WriteString(boardStyle.Render("┃"))
     }
